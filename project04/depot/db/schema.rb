f233c4b56e9aa2a08bdebb65d6a754cb9617542a
Billy Dixon <bidixon@mymail.mines.edu>
2013-03-08 16:53:54 -0700

    Added migration for combining items in cart

--- project04/depot/db/schema.rb
+++ project04/depot/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130308234141) do
+ActiveRecord::Schema.define(:version => 20130308234742) do
 
   create_table "carts", :force => true do |t|
     t.datetime "created_at", :null => false
